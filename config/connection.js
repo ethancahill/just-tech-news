67caf26e03e6057ad741a9c109e946b468a496af
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-05-21 19:23:02 -0400

    established CRUD

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,12 @@
-// import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 
-require('dotenv').config()
+require('dotenv').config();
 
-// create connection to our database, pass in your MySQL information for username and password
-const sequelize = new Sequilize('just_tech_news_db', 'username', 'password', {
-    host: '127.0.0.1',
-    dialect: 'mysql',
-    port: 3306
+// create connection to our db
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
 });
 
 module.exports = sequelize;
